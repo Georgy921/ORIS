c3595fcf70f01ab217daee1c4a401386937fc52f
Georgy Mikhaylov <aleksysnmpov@gmail.com>
2025-12-06 00:42:58 +0300

    Hw4

---
+++ HttPServer/Core/Attributes/Endpoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttPServer.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class Endpoint : Attribute
+    {
+        public Endpoint() { }
+    }
+}
---
+++ HttPServer/Core/Attributes/HttpGet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttPServer.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HttpGet : Attribute
+    {
+        public string? Route { get; }
+        public HttpGet()
+        {
+        }
+
+        public HttpGet(string? route)
+        {
+            Route = route;
+        }
+    }
+}
