671273641368a323ebb1794883e060fbc9f85fa0
Georgy Mikhaylov <aleksysnmpov@gmail.com>
2025-12-17 23:54:33 +0300

    hw9

---
+++ Homework_9/XProtocol/XPacketHandshake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XProtocol.Serializator;
+
+namespace XProtocol
+{
+    public class XPacketHandshake
+    {
+        [XField(1)]
+        public string Username;
+
+        [XField(2)]
+        public string ColorHex;
+    }
+}
